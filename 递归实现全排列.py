298fe039683aa311dfa8adeb889e7fc4bcaa82e1
siyizhou <48543747+siyizhou@users.noreply.github.com>
2019-04-21 23:01:26 +0800

    Update 递归实现全排列.py

--- 递归实现全排列.py
+++ 递归实现全排列.py
@@ -4,3 +4,17 @@
 #排列：从n个元素中任取m个元素，并按照一定的顺序进行排列，称为排列
 #全排列：当n==m时，称为全排列
 
+class perm:
+    def perm_function(self,list1,s=""):
+        if type(list1)!=type([]):
+            return
+        if len(list1)==0:  #参数合法性检验
+            return
+        if len(list1)==1:     #跳出条件
+            print(s+list1[0])   #打印当前结果
+            return 
+        for i,e in enumerate(list1):
+            self.perm_function(list1[:i]+list1[i+1:],s+e)
+            
+f=perm()
+f.perm_function(["1","2","3"])
